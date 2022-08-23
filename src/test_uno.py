c425e1486e8d719e7d6225cf68e4fdd6875d3856
Chad Estioco <chadestioco@gmail.com>
2022-08-23 21:45:09 +0200

    Add draw method.

--- src/test_uno.py
+++ src/test_uno.py
@@ -1,4 +1,26 @@
-from .uno import UnoDeck
+from .uno import UnoDeck, UnoCardType
 
-def test_deck_initial_state():
-    assert len(UnoDeck()) == 108
+import unittest
+import unittest.mock
+
+class UnoDeckTests(unittest.TestCase):
+
+    def setUp(self):
+        self.deck = UnoDeck()
+
+    def test_deck_initial_state(self):
+        self.assertEqual(len(self.deck), 108)
+
+    @unittest.mock.patch("random.choice")
+    def test_draw(self, choice_mock):
+        choice_mock.return_value = UnoCardType.RED_0
+        self.assertEqual(choice_mock.return_value, self.deck.draw())
+        self.assertEqual(0, self.deck.deck[choice_mock.return_value])
+        self.assertTrue(choice_mock.return_value not in self.deck.non_zeros)
+
+    def test_draw_to_end(self):
+        for _ in range(108):
+            self.deck.draw()
+
+        self.assertIsNone(self.deck.draw())
+        self.assertEqual(0, len(self.deck))
--- src/uno.py
+++ src/uno.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, Set
 
 import enum
+import random
 
 CARD_COLORS = ("RED", "YELLOW", "GREEN", "BLUE")
 ACTION_CARDS = ("SKIP", "DRAWTWO", "REVERSE")
@@ -23,20 +24,45 @@ def create_cardtype_enum():
 UnoCardType = create_cardtype_enum()
 
 class UnoDeck(object):
+    """
+    Stateful representation of an Uno Deck. Each method is constant time
+    (i.e., Big-O is bounded by the 108 card count, never an integral multiple).
+    """
 
     def __init__(self):
         self.deck: Dict[UnoCardType, int] = {}
         self.deck[UnoCardType.WILD] = 4
         self.deck[UnoCardType.WILDFOUR] = 4
+        self.non_zeros: Set[UnoCardType] = set([UnoCardType.WILD, UnoCardType.WILDFOUR])
 
         for color in CARD_COLORS:
-            self.deck["%s_0" % color] = 1
+            card_zero = UnoCardType["%s_0" % color]
+            self.deck[card_zero] = 1
+            self.non_zeros.add(card_zero)
 
             for n in range(1, 10):
-                self.deck["%s_%d" % (color, n)] = 2
+                card_n = UnoCardType["%s_%d" % (color, n)]
+                self.deck[card_n] = 2
+                self.non_zeros.add(card_n)
 
             for act in ACTION_CARDS:
-                self.deck["%s_%s" % (color, act)] = 2
+                action_card = UnoCardType["%s_%s" % (color, act)]
+                self.deck[action_card] = 2
+                self.non_zeros.add(action_card)
 
     def __len__(self):
         return sum([self.deck[k] for k in self.deck.keys()])
+
+    def draw(self):
+        """
+        Randomly gets a card from the deck.
+        """
+        if self.non_zeros:
+            card = random.choice(list(self.non_zeros))
+            self.deck[card] -= 1
+            if not self.deck[card]:
+                self.non_zeros.remove(card)
+
+            return card
+        else:
+            return None
